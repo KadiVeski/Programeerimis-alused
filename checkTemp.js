e398de029874ef18fd639d3b64bcd33d95f42796
KadiVeski <115346268+KadiVeski@users.noreply.github.com>
2022-10-14 20:06:51 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -8,7 +8,7 @@ function checkTemp(temp) {
     var temperatuur = 1000;
     if(temp>=0 && temp<=20) {
     temperatuur = -1
- //       document.write('jeeee');
+ 
 
     
     } else if(temp>=21 && temp<=40) {
